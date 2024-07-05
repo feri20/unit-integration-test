da55a5405346724898467416b914ad5ca3328c5e
feri20 <f_naderi66@yahoo.com>
2024-07-05 12:39:16 +0330

    Replace @SpringBootTest with @ExtendWith()

--- src/test/java/com/example/unit/product/unit/ProductControllerUnitTest.java
+++ src/test/java/com/example/unit/product/unit/ProductControllerUnitTest.java
@@ -7,8 +7,8 @@ import com.example.unit.ProductService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -21,7 +21,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductControllerUnitTest {
-    @MockBean
+    @Mock
     private ProductService productService;
     @InjectMocks
     private ProductController productController;
